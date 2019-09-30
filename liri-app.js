599f2cafd789557bdf243eb8e3ff423fa6179cc3
Deya Genao <deyagenao@gmail.com>
2019-09-30 12:49:03 -0400

    updated the spotify search so that a default result would appear if no search query provided

--- liri-app.js
+++ liri-app.js
@@ -24,6 +24,9 @@ var spotify = new Spotify(keys.spotify);
 var searchCommand = process.argv[2];
 var searchQuery = process.argv.slice(3).join("+");
 
+// variable for the search query to display in the console, along with results 
+var searchQueryForPrint = process.argv.slice(3).join(" ");
+
 // Switch function to determine which search will run, based on the 
 switch(searchCommand){
     case "concert-this":
@@ -57,30 +60,34 @@ switch(searchCommand){
 // function connected to the `concert-this`command 
 // `node liri.js concert-this <artist/band name here>`
 function searchBandsInTown(){
-    console.log("Hey");
     // check to see if the user input the name of an artist or band 
     if (searchQuery){
         // if the user did insert an actual search query, then run the axios request to the Bands in Town api
         axios.get("https://rest.bandsintown.com/artists/" + searchQuery + "/events?app_id=codingbootcamp")
         .then(function(response){
-            console.log(response);
-            for (var i=0; i < response.data.length; i++){
-                var eventDate = moment(response.data[i].datetime, "YYYY-MM-DDTHH:mm:ss").format("MM/DD/YYYY");
-                console.log(
-                    // * Name of the venue
-                    // * Venue location
-                    // * Date of the Event
-                    "\nName of Venue: " + response.data[i].venue.name+
-                    "\nVenue location: " + response.data[i].venue.city+ ", " + response.data[i].venue.country+ 
-                    "\nEvent Date: " +eventDate+ 
-                    "\n--------------------------------------------------------------"
-                )
+            console.log("Showing event results for " + searchQueryForPrint + ":");
+            // check to see if there are any upcoming events 
+            if (response.data.length > 0) {
+                for (var i=0; i < response.data.length; i++){
+                    var eventDate = moment(response.data[i].datetime, "YYYY-MM-DDTHH:mm:ss").format("MM/DD/YYYY");
+                    console.log(
+                        // * Name of the venue
+                        // * Venue location
+                        // * Date of the Event
+                        "\nName of Venue: " + response.data[i].venue.name+
+                        "\nVenue location: " + response.data[i].venue.city+ ", " + response.data[i].venue.country+ 
+                        "\nEvent Date: " +eventDate+ 
+                        "\n--------------------------------------------------------------"
+                    )
+                }
+            }else{
+                console.log("There are no upcoming events.")
             }
         });
     }else{
         axios.get("https://rest.bandsintown.com/artists/cardi+b/events?app_id=codingbootcamp")
         .then(function(response){
-            console.log(response);
+            // console.log(response);
             console.log("Showing event results for Cardi B: ") 
             for (var i=0; i < response.data.length; i++){
                 var eventDate = moment(response.data[i].datetime, "YYYY-MM-DDTHH:mm:ss").format("MM/DD/YYYY");
@@ -98,34 +105,34 @@ function searchBandsInTown(){
     }
 }
 
-
-// * This will search the Bands in Town Artist Events API (`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"`) for an artist and render the following information about each event to the terminal:
-
-// * Name of the venue
-
-// * Venue location
-
-// * Date of the Event (use moment to format this as "MM/DD/YYYY")
-
 // `spotify-this-song`
-// 2. `node liri.js spotify-this-song '<song name here>'`
+// `node liri.js spotify-this-song '<song name here>'`
+function searchSpotify() {
+    if (searchQuery){
+        spotify.search({type: "track", query: searchQuery}, function(err,response){
+            if (err){
+                return console.log("An error occurred: " + err);
+            }
+            console.log(JSON.stringify(response));
+        })
+    }else{
+        spotify.search({type: 'track', query: 'The Sign'}, function (err, response) {
+            if (err) {
+                return console.log("An error occurred: " + err);
+            }
+            var songName = response.tracks.items[5].name;
+            var artistName = response.tracks.items[5].artists[0].name; 
+            var songLink = response.tracks.items[5].external_urls.spotify;
+            var album = response.tracks.items[5].album.name;
 
-//    * This will show the following information about the song in your terminal/bash window
-
-//      * Artist(s)
-
-//      * The song's name
-
-//      * A preview link of the song from Spotify
-
-//      * The album that the song is from
-
-//    * If no song is provided then your program will default to "The Sign" by Ace of Base.
-
-//    * You will utilize the [node-spotify-api](https://www.npmjs.com/package/node-spotify-api) package in order to retrieve song information from the Spotify API.
+            console.log('Song name: "'+songName+ '" \nArtist(s): ' + artistName +
+            '\nAlbum: ' + album + '\nLink to the song on Spotify: ' + songLink);
+        })
+    }
+}
 
 
-//  `movie-this`
+//  NEW COMMAND: `movie-this`
 // `node liri.js movie-this '<movie name here>'
 function searchOMDB(){
     console.log("woo!");
@@ -157,8 +164,8 @@ function searchOMDB(){
     };
 }
 
-//  `do-what-it-says`
-// 4. `node liri.js do-what-it-says`
+// NEW COMMAND `do-what-it-says`
+// `node liri.js do-what-it-says`
 function searchRandomFile() {
     fs.readFile("random.txt", "utf8", function(error, data) {
         if (error) {
@@ -167,7 +174,7 @@ function searchRandomFile() {
         // testing to log the data
         console.log(data);
         
-        // create an array from the data to separate the command from the actual search 
+        // create an array from the data read from the random file to separate the command from the actual search 
         var dataArray = data.split(",");
 
         console.log(dataArray);
@@ -178,7 +185,6 @@ function searchRandomFile() {
         if (searchCommand === "movie-this"){
             if (searchQuery){
                 axios.get("http://www.omdbapi.com/?t=" + searchQuery + "&apikey=trilogy").then(function(response){
-                    // console.log(response.data);
                     console.log("Title: " + response.data.Title + 
                     "\nYear: " + response.data.Year+ 
                     "\nIMDB Rating: " + response.data.Ratings[0].Value+
